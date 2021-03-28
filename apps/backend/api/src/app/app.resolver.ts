a4136a61cb7405ab2e9be41ffaac4116acdaf2ed
iromashko <iromashko@me.com>
2021-03-28 16:44:24 +0300

    add graphql module

---
+++ apps/backend/api/src/app/app.resolver.ts
@@ -0,0 +1,9 @@
+import { Query, Resolver } from '@nestjs/graphql';
+
+@Resolver('app')
+export class AppResolver {
+  @Query('test')
+  async getData() {
+    return `It's GraphQL response`;
+  }
+}
